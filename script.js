d556e1095c216ee8cb22312ba10160cbdcaa9840
jiteshcb1 <jitesh.chhabra@outlook.com>
2022-05-04 23:08:43 +0530

    Icon Change

--- script.js
+++ script.js
@@ -5,24 +5,16 @@ const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const tabBtn = document.getElementById("tab-btn")
 
-//get the leads from the localStorage - PS: JSON.parse()
-//Store it in a variable, leadsFromLocalStorage
-//Log out the variable
 
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-
-//check if leadsfromLocalStorage is truthy
-//Is So, set myLeads to its value and call renderLeads()
 
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-// listen for clicks on tabBtn. grab the tab URL
-
 tabBtn.addEventListener("click", function(){
-    // Grab the URL of the current tab
+
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
@@ -33,11 +25,10 @@ tabBtn.addEventListener("click", function(){
 function render(leads){
     let listItems = ""
     for(let i = leads.length - 1; i >= 0; i--){
-        // listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>"
         listItems += `
             <li>
                 <a href='${leads[i]}' target='_blank'>
-                    ${leads[i]}
+                    ${leads[i]} 
                 </a>
             </li>
         `
@@ -46,8 +37,6 @@ function render(leads){
 }
 
 
-//listen for double clicks on the delete button 
-//when clicked, clear localStorage, myLeads, and 
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
@@ -62,10 +51,3 @@ inputBtn.addEventListener("click", function() {
     render(myLeads)
 
 })
-
-
-
-// let date = d.getHours() + ":" + d.getMinutes() + " " + d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear()
-
-// for putting two different text on opposite ends
-//https://stackoverflow.com/questions/40091515/how-to-have-two-items-on-opposite-sides-on-the-same-line
