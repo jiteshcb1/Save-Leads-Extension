60a56f22c4e73d6a46cfac7cc308eef3bc7d6048
jiteshcb1 <jitesh.chhabra@outlook.com>
2022-05-05 22:06:30 +0530

    Added Date and UI fixes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLeads = []
+let date = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -7,29 +8,41 @@ const tabBtn = document.getElementById("tab-btn")
 
 
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const dateFromLocalStorage = JSON.parse( localStorage.getItem("date"))
 
-if(leadsFromLocalStorage){
+if(leadsFromLocalStorage && dateFromLocalStorage){
+    date = dateFromLocalStorage
     myLeads = leadsFromLocalStorage
-    render(myLeads)
+    render(myLeads, date)
 }
 
 tabBtn.addEventListener("click", function(){
-
+    date.push(new Date().toLocaleString())
+    localStorage.setItem("date", JSON.stringify(date))
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
+        render(myLeads, date)
     })
 })
 
-function render(leads){
+function render(leads, date){
     let listItems = ""
     for(let i = leads.length - 1; i >= 0; i--){
         listItems += `
             <li>
-                <a href='${leads[i]}' target='_blank'>
-                    ${(i+1)+". "} ${leads[i]} 
-                </a>
+                <div id = "flex">
+                    <span>
+                        <span id = "clr">${(i+1) + ". "}</span>
+                        <a href='${leads[i]}' target='_blank'>
+                            ${leads[i]}
+                        </a>
+                    </span>
+                    <span id = "clr">
+                        ${date[i]} 
+                    </span>
+
+                </div>
             </li>
         `
     }
@@ -45,9 +58,11 @@ deleteBtn.addEventListener("dblclick", function() {
 })
 
 inputBtn.addEventListener("click", function() {
+    date.push(new Date().toLocaleString())
+    localStorage.setItem("date", JSON.stringify(date))
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    render(myLeads, date)
 
 })
